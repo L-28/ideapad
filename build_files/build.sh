e60bab099b1b2033b1958810293c4c6af7f0b4ac
Leonidas Adaktylos <147805834+L-28@users.noreply.github.com>
2025-11-28 15:11:08 +0100

    Initial Setup

---
+++ build_files/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
+dnf5 install -y --skip-unavailable fish micro netcat fastfetch bsd-games rogue arp-scan evtest \
+		cargo gcc telnet adw-gtk3-theme input-remapper steam  \
+		waypipe
+
+# Use a COPR Example:
+#
+dnf5 -y copr enable starfish/howdy-beta
+dnf5 -y install howdy howdy-gtk
+# Disable COPRs so they don't end up enabled on the final image:
+dnf5 -y copr disable starfish/howdy-beta
+
+#### Example for enabling a System Unit File
+
+#chmod 666 /dev/video2
