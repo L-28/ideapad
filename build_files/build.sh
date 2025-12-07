ff39264cb095c36be7278723a05087a09f573540
Leonidas Adaktylos <leonidas@adaktylos.net>
2025-12-07 12:31:16 +0100

    Added IBM Plex fonts

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y --skip-unavailable fish micro netcat fastfetch bsd-games rogue \
 		arp-scan evtest telnet adw-gtk3-theme input-remapper steam  \
-		waypipe nautilus-gsconnect
+		waypipe nautilus-gsconnect ibm-plex-fonts-all
 
 # Use a COPR Example:
 #
