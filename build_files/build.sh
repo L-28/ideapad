9c925b7ca155d2cb558fbc03517e133c4bf37539
Leonidas Adaktylos <leonidas@adaktylos.net>
2025-12-08 20:50:48 +0100

    Switch to rpm-ostree install in hopes of getting rid of dnf

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,14 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y --skip-unavailable fish micro netcat fastfetch bsd-games rogue \
+rpm-ostree install -yA fish micro netcat fastfetch bsd-games rogue \
 		arp-scan evtest telnet adw-gtk3-theme input-remapper steam  \
 		waypipe nautilus-gsconnect ibm-plex-fonts-all \
 		gnome-software-rpm-ostree mozilla-openh264
 
 # Use a COPR Example:
 #
-dnf5 -y copr enable starfish/howdy-beta
-dnf5 -y install howdy howdy-gtk
+rpm-ostree install -yA --rpm-repo=https://copr.fedorainfracloud.org/coprs/starfish/howdy-beta/ howdy howdy-gtk
 # Disable COPRs so they don't end up enabled on the final image:
 dnf5 -y copr disable starfish/howdy-beta
 
@@ -26,3 +25,5 @@ dnf5 -y copr disable starfish/howdy-beta
 
 #chmod 666 /dev/video2
 systemctl enable podman.socket input-remapper
+
+rpm-ostree override remove 'dnf*' 'libdnf*' python3-dnf python3-dnf5
