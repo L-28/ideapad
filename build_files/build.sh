69d34337fc9a39d65ebf26c6fdcf70ff24f6cb29
Leonidas Adaktylos <leonidas@adaktylos.net>
2025-12-08 21:38:18 +0100

    Fixing Steam and adding mpd

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,16 +14,20 @@ rpm-ostree install -yA fish micro netcat fastfetch bsd-games rogue \
 		arp-scan evtest telnet adw-gtk3-theme input-remapper steam  \
 		waypipe nautilus-gsconnect ibm-plex-fonts-all \
 		gnome-software-rpm-ostree mozilla-openh264
+rpm-ostree install -yA \
+  --rpm-repo=https://download1.rpmfusion.org/free/fedora/releases/43/Everything/x86_64/os \
+  --rpm-repo=https://download1.rpmfusion.org/nonfree/fedora/releases/43/Everything/x86_64/os/ \
+  steam steam-devices kernel-modules-extra mpd
+
 
 # Use a COPR Example:
 #
 rpm-ostree install -yA --rpm-repo=https://copr.fedorainfracloud.org/coprs/starfish/howdy-beta/ howdy howdy-gtk
 # Disable COPRs so they don't end up enabled on the final image:
-dnf5 -y copr disable starfish/howdy-beta
 
 #### Example for enabling a System Unit File
 
 #chmod 666 /dev/video2
-systemctl enable podman.socket input-remapper
+systemctl enable podman.socket input-remapper mpd
 
 rpm-ostree override remove 'dnf*' 'libdnf*' python3-dnf python3-dnf5
