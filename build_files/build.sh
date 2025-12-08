7fd7a55a18ab0b1546a69985bfb3f9477b9bce32
Leonidas Adaktylos <leonidas@adaktylos.net>
2025-12-08 23:37:16 +0100

    Fixed rpm-fusion and changed the syntax slightly

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,16 +9,16 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-sudo rpm-ostree install \
+dnf5 install -y \
 	https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # this installs a package from fedora repos
-dnf5 install -y fish micro netcat fastfetch bsd-games rogue \
-		arp-scan evtest telnet adw-gtk3-theme input-remapper \
-		waypipe nautilus-gsconnect ibm-plex-fonts-all \
-		gnome-software-rpm-ostree mozilla-openh264 steam-devices \
-		steam mpd
+dnf5 install -y \
+	fish micro netcat fastfetch bsd-games rogue steam mpd \
+	arp-scan evtest telnet adw-gtk3-theme input-remapper \
+	waypipe nautilus-gsconnect ibm-plex-fonts-all \
+	gnome-software-rpm-ostree mozilla-openh264 steam-devices
 
 # Use a COPR Example:
 dnf5 -y copr enable starfish/howdy-beta
