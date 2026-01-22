c29031706148af38a074881a7071daf44ac59a07
Leonidas Adaktylos <leonidas@adaktylos.net>
2026-01-22 16:44:02 +0100

    Added bign-handheld-thumbnailer

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,7 +34,8 @@ dnf5 -y --refresh install --skip-unavailable \
 	telnet adw-gtk3-theme input-remapper waypipe ibm-plex-fonts-all \
 	steam-devices gnome-software-rpm-ostree	mozilla-openh264 howdy \
 	howdy-gtk rpmfusion-free-release-tainted libdvdcss gcc \
-	gnome-terminal-nautilus seahorse-nautilus nautilus-gsconnect
+	gnome-terminal-nautilus seahorse-nautilus nautilus-gsconnect \
+	bign-handheld-thumbnailer
 
 # Use a COPR Example:
 # rpm-ostree -y copr enable starfish/howdy-beta
